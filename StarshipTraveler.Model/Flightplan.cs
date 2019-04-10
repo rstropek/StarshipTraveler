97bbcfcd4b933e75369b9e937302dacb3cb5c171
Rainer Stropek <rainer@software-architects.at>
2019-04-10 16:31:02 +0200

    Add Starship Traveler sample

---
+++ StarshipTraveler.Model/Flightplan.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace StarshipTraveler.Model
+{
+    public class BasePoint
+    {
+        public Base Base { get; set; }
+        public ConnectionWithBases[] DirectConnections { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public bool Active { get; private set; }
+        public void SetActive(bool active = true)
+        {
+            foreach (var conn in DirectConnections)
+            {
+                conn.Active = active;
+                conn.To.Active = active;
+            }
+
+            Active = active;
+        }
+    }
+
+    public class ConnectionWithBases
+    {
+        public BasePoint From { get; set; }
+        public BasePoint To { get; set; }
+        public (double pFromX, double pFromY, double pToX, double pToY) ControlPoints { get; set; }
+        public Connection Connection { get; set; }
+        public bool Active { get; set; }
+    }
+
+    public interface IFlightplan
+    {
+        (BasePoint[] bases, ConnectionWithBases[] connections) PrepareFlightplan(Connection[] connectionTask, Base[] baseTask, (double X, double Y) center);
+    }
+
+    public class Flightplan : IFlightplan
+    {
+        public (BasePoint[] bases, ConnectionWithBases[] connections) PrepareFlightplan(Connection[] connectionTask, Base[] baseTask, (double X, double Y) center)
+        {
+            var angleStep = Math.PI * 2 / baseTask.Length;
+            var points = Enumerable.Range(0, baseTask.Length)
+                .Select(i => new BasePoint
+                {
+                    Base = baseTask[i],
+                    X = 250 + Math.Sin(angleStep * i) * 250,
+                    Y = 250 - Math.Cos(angleStep * i) * 250
+                })
+                .ToArray();
+            var connections = connectionTask.Select(c => new ConnectionWithBases
+            {
+                Connection = c,
+                From = points.First(b => b.Base.Name == c.From),
+                To = points.First(b => b.Base.Name == c.To),
+            }).ToArray();
+            foreach(var conn in connections)
+            {
+                conn.ControlPoints = (conn.From.X + (center.X - conn.From.X) / 2,
+                    conn.From.Y + (center.Y - conn.From.Y) / 2,
+                    conn.To.X + (center.X - conn.To.X) / 2,
+                    conn.To.Y + (center.Y - conn.To.Y) / 2);
+            }
+
+            foreach (var point in points)
+            {
+                point.DirectConnections = connections.Where(c => c.From == point).ToArray();
+            }
+
+            return (points, connections);
+        }
+    }
+}
---
+++ StarshipTraveler.Model/Model.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StarshipTraveler.Model
+{
+    public class Base
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class Ticket
+    {
+        [Required]
+        public DateTime Date { get; set; }
+
+        public string ID { get; set; }
+
+        [Required]
+        public string From { get; set; }
+
+        [Required]
+        public string To { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 50 characters!")]
+        public string Passenger { get; set; }
+
+        public TimeRelation Time => Date >= DateTime.Today ? TimeRelation.Upcoming : TimeRelation.Past;
+    }
+
+    public enum TimeRelation
+    {
+        Past,
+        Upcoming
+    }
+
+    public class Connection
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Distance { get; set; }
+        public decimal Price { get; set; }
+    }
+}
