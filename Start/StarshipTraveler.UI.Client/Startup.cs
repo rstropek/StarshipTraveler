dbf546892d5cc70562777175d98f9d2ef4961d8e
Rainer Stropek <rainer@software-architects.at>
2019-04-11 09:06:11 +0200

    Minor addition to Start solution

--- Start/StarshipTraveler.UI.Client/Startup.cs
+++ Start/StarshipTraveler.UI.Client/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+// using StarshipTraveler.Model;
 
 namespace StarshipTraveler.UI.Client
 {
@@ -7,6 +8,7 @@ namespace StarshipTraveler.UI.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddSingleton<IFlightplan, Flightplan>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
