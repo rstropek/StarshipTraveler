30ac0a647511cab9e97e41c530cb113bc176dc61
Rainer Stropek <rainer@software-architects.at>
2019-05-20 22:09:15 +0200

    Update StarshipTraveler sample to latest Blazor preview

--- StarshipTraveler.Components/FlightNetwork.cs
+++ StarshipTraveler.Components/FlightNetwork.cs
@@ -21,8 +21,8 @@ namespace StarshipTraveler.Components
 
         protected override async Task OnInitAsync()
         {
-            var connectionTask = Http.GetJsonAsync<Connection[]>("/api/connections");
-            var baseTask = Http.GetJsonAsync<Base[]>("/api/bases");
+            var connectionTask = Http.GetJsonAsync<Connection[]>("api/connections");
+            var baseTask = Http.GetJsonAsync<Base[]>("api/bases");
             await Task.WhenAll(connectionTask, baseTask);
             (Points, Connections) = Flightplan.PrepareFlightplan(connectionTask.Result, baseTask.Result, (250d, 250d));
         }
@@ -30,48 +30,47 @@ namespace StarshipTraveler.Components
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
             base.BuildRenderTree(builder);
-            var seq = 0;
 
             if (Points != null && Connections != null)
             {
-                builder.OpenElement(seq, "svg");
-                builder.AddAttribute(++seq, "class", "flightplan-svg");
-                builder.AddAttribute(++seq, "viewBox", "0 0 600 600");
-                builder.AddAttribute(++seq, "preserveAspectRatio", "xMinYMin");
+                builder.OpenElement(1, "svg");
+                builder.AddAttribute(2, "class", "flightplan-svg");
+                builder.AddAttribute(3, "viewBox", "0 0 600 600");
+                builder.AddAttribute(4, "preserveAspectRatio", "xMinYMin");
 
-                builder.OpenElement(++seq, "g");
-                builder.AddAttribute(++seq, "class", "flightplan");
+                builder.OpenElement(5, "g");
+                builder.AddAttribute(6, "class", "flightplan");
 
                 foreach (var conn in Connections)
                 {
-                    builder.OpenElement(++seq, "path");
-                    builder.AddAttribute(++seq, "d", $"M {conn.From.X},{conn.From.Y} C {conn.ControlPoints.pFromX},{conn.ControlPoints.pFromY} {conn.ControlPoints.pToX},{conn.ControlPoints.pToY} {conn.To.X},{conn.To.Y}");
-                    builder.AddAttribute(++seq, "class", conn.Active ? "active" : "");
+                    builder.OpenElement(7, "path");
+                    builder.AddAttribute(8, "d", $"M {conn.From.X},{conn.From.Y} C {conn.ControlPoints.pFromX},{conn.ControlPoints.pFromY} {conn.ControlPoints.pToX},{conn.ControlPoints.pToY} {conn.To.X},{conn.To.Y}");
+                    builder.AddAttribute(9, "class", conn.Active ? "active" : "");
                     builder.CloseElement();
                 }
 
                 foreach (var point in Points)
                 {
-                    builder.OpenElement(++seq, "circle");
-                    builder.AddAttribute(++seq, "r", 5);
-                    builder.AddAttribute(++seq, "cx", point.X);
-                    builder.AddAttribute(++seq, "cy", point.Y);
-                    builder.AddAttribute(++seq, "class", "pulse-circle " + (point.Active ? "active" : "inactive"));
+                    builder.OpenElement(10, "circle");
+                    builder.AddAttribute(11, "r", 5);
+                    builder.AddAttribute(12, "cx", point.X);
+                    builder.AddAttribute(13, "cy", point.Y);
+                    builder.AddAttribute(14, "class", "pulse-circle " + (point.Active ? "active" : "inactive"));
                     builder.CloseElement();
 
-                    builder.OpenElement(++seq, "text");
-                    builder.AddAttribute(++seq, "x", point.X + 10);
-                    builder.AddAttribute(++seq, "y", point.Y + 10);
-                    builder.AddContent(++seq, point.Base.Name);
+                    builder.OpenElement(15, "text");
+                    builder.AddAttribute(16, "x", point.X + 10);
+                    builder.AddAttribute(17, "y", point.Y + 10);
+                    builder.AddContent(18, point.Base.Name);
                     builder.CloseElement();
 
-                    builder.OpenElement(++seq, "circle");
-                    builder.AddAttribute(++seq, "r", 5);
-                    builder.AddAttribute(++seq, "fill", "white");
-                    builder.AddAttribute(++seq, "cx", point.X);
-                    builder.AddAttribute(++seq, "cy", point.Y);
-                    builder.AddAttribute(++seq, "onmouseenter", EventCallback.Factory.Create<UIMouseEventArgs>(this, new Action(() => point.SetActive())));
-                    builder.AddAttribute(++seq, "onmouseleave", EventCallback.Factory.Create<UIMouseEventArgs>(this, new Action(() => point.SetActive(false))));
+                    builder.OpenElement(19, "circle");
+                    builder.AddAttribute(20, "r", 5);
+                    builder.AddAttribute(21, "fill", "white");
+                    builder.AddAttribute(22, "cx", point.X);
+                    builder.AddAttribute(23, "cy", point.Y);
+                    builder.AddAttribute(24, "onmouseenter", EventCallback.Factory.Create<UIMouseEventArgs>(this, new Action(() => point.SetActive())));
+                    builder.AddAttribute(25, "onmouseleave", EventCallback.Factory.Create<UIMouseEventArgs>(this, new Action(() => point.SetActive(false))));
                     builder.CloseElement();
                 }
 
