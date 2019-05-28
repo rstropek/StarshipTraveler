4c75874f3a557a31223824a9af495e5156343c8f
Rainer Stropek <rainer@software-architects.at>
2019-05-28 09:45:27 +0200

    Add server-side version of StarshipTraveler

---
+++ StartshipTraveler.ServerSide/Controllers/ConnectionsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using StarshipTraveler.UI.Server.Data;
+using StarshipTraveler.Model;
+
+namespace StarshipTraveler.UI.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConnectionsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Connection> Connections() =>
+            SampleData.Connections.Union(SampleData.Connections.Select(c => new Connection
+                {
+                    From = c.To,
+                    To = c.From,
+                    Distance = c.Distance,
+                    Price = c.Price
+                })).ToArray();
+    }
+}
---
+++ StartshipTraveler.ServerSide/Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StarshipTraveler.Model;
+
+namespace StartshipTraveler.ServerSide
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<IFlightplan, Flightplan>();
+            services.AddSingleton((_) => new HttpClient() { BaseAddress = new Uri("http://localhost:61412/", UriKind.Absolute) });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
